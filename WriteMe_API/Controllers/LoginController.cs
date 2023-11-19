d50622f912cb3b65f1dd502c0971296ff984b4da
Francina Dippiton <Francinagio@hotmail.com>
2023-11-19 13:15:14 -0400

    Setting JWT

---
+++ WriteMe_API/Controllers/LoginController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NuGet.Protocol;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WriteMe_API.Models;
+using WriteMe_API.ViewModels;
+
+namespace WriteMe_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly WriteMeContext _context;
+        private readonly IConfiguration _config;
+
+        public LoginController(WriteMeContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _config = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetNewCurrentUser()
+        {
+            var currentUser = GetCurrentUser();
+            if (currentUser != null)
+            {
+                return Ok($"Hola {currentUser.UsuNombre}, tu correo es {currentUser.UsuCorreo}");
+            }
+            else
+            {
+                return NotFound("Usuario no encontrado");
+            }
+
+            
+            //return Ok(currentUser);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginUser userLogin)
+        {
+            try
+            {
+                var user = Authenticate(userLogin);
+
+                if (user != null)
+                {
+                    var token = Generate(user);
+                    return Ok(token);
+                }
+
+                return NotFound("No se pudo loguear. Usuario no encontrado");
+            }
+            catch (Exception ex)
+            {
+                // Manejar la excepción según tus necesidades
+                Console.WriteLine($"Error en el método Login: {ex}");
+                return StatusCode(500, "Error interno del servidor");
+                // Puedes devolver un código de estado 500 (Internal Server Error) o manejar la excepción de otra manera
+            }
+        }
+
+
+        private Usuario Authenticate(LoginUser userLogin)
+        {
+
+            try
+            {
+                var currentUser = _context.Usuarios.FirstOrDefault(user => user.UsuCorreo == userLogin.UsuCorreo
+                    && user.UsuContrasena == userLogin.UsuContrasena);
+
+                if (currentUser != null)
+                {
+                    Console.WriteLine(currentUser.ToJson());
+                    return currentUser;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Aquí puedes manejar la excepción, imprimir un mensaje de registro, etc.
+                // Puedes personalizar este bloque catch según tus necesidades específicas.
+                Console.WriteLine($"Error en Authenticate: {ex.Message}");
+            }
+
+            return null;
+        }
+
+        private string Generate(Usuario user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Crear claims
+            var claims = new[]
+            {
+                new Claim("UsuId", user.UsuId.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.UsuNombre),
+                new Claim(ClaimTypes.Email, user.UsuCorreo),
+                new Claim("UsuStatus", user.UsuStatus),
+            };
+
+            // Crear el token
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private Usuario GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+
+                var usuIdClaim = identity.FindFirst("UsuId");
+                if (usuIdClaim != null && int.TryParse(usuIdClaim.Value, out var usuId))
+                {
+                    // Cargar el usuario desde el contexto o la base de datos utilizando el ID del usuario
+                    var currentUser = _context.Usuarios.FirstOrDefault(u => u.UsuId == usuId);
+
+                    if (currentUser != null)
+                    {
+                        return new Usuario
+                        {
+                            UsuNombre = currentUser.UsuNombre,
+                            UsuCorreo = currentUser.UsuCorreo,
+                            UsuStatus = currentUser.UsuStatus,
+                        };
+                    }
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ WriteMe_API/ViewModels/LoginUser.cs
@@ -0,0 +1,9 @@
+﻿namespace WriteMe_API.ViewModels
+{
+    public class LoginUser
+    {
+        public string UsuCorreo { get; set; } = null!;
+
+        public string UsuContrasena { get; set; } = null!;
+    }
+}
