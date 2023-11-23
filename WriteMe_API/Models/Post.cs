6ef6e111a7e11321ca04b32e5a5cc25dc12685fb
Francina Dippiton <Francinagio@hotmail.com>
2023-11-23 18:11:46 -0400

    Setting edit post

--- WriteMe_API/Models/Post.cs
+++ WriteMe_API/Models/Post.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace WriteMe_API.Models;
 
 public partial class Post
 {
     public int PostId { get; set; }
-
+    [DisplayName("Titulo")]
     public string PostTitulo { get; set; } = null!;
-
+    [DisplayName("Contenido")]
     public string PostContenido { get; set; } = null!;
 
     public DateTime PostFechaPublicacion { get; set; }
 
     public int? PostUsuarioId { get; set; }
-
+    [DisplayName("Categoria")]
     public int? PostCategoria { get; set; }
 
     public string PostStatus { get; set; } = null!;
--- WriteMe_MVC/Controllers/PostsController.cs
+++ WriteMe_MVC/Controllers/PostsController.cs
@@ -1,20 +1,56 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using WriteMe_MVC.Models;
+using WriteMe_MVC.ViewModels;
 
 namespace WriteMe_MVC.Controllers
 {
     public class PostsController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient httpClient;
+
+
+        public PostsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            httpClient = new HttpClient();
+        }
+
         // GET: PostsController
         public ActionResult Index()
         {
             return View();
         }
 
+        // Obtener los detalles de un post especifico
         // GET: PostsController/Details/5
-        public ActionResult Details(int id)
+        [HttpGet("PostDetalles/{id}")]
+        public async Task <ActionResult> PostDetails(int id)
         {
-            return View();
+            // Especificar la URL del endpoint de la API
+            string baseApiUrl = _configuration.GetSection("WriteMeApi").Value!;
+
+            PostViewModel postInfo = new PostViewModel();
+            using (var client = new HttpClient())
+            {
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage Res = await client.GetAsync($"{baseApiUrl}/posts/" + id.ToString());
+
+                if (Res.IsSuccessStatusCode)
+                {
+                    var EquiResponse = Res.Content.ReadAsStringAsync().Result;
+                    postInfo = JsonConvert.DeserializeObject<PostViewModel>(EquiResponse)!;
+                }
+
+                return View(postInfo);
+            }
         }
 
         // GET: PostsController/Create
@@ -39,15 +75,43 @@ namespace WriteMe_MVC.Controllers
         }
 
         // GET: PostsController/Edit/5
-        public ActionResult Edit(int id)
+        [HttpGet]
+        public async Task<ActionResult> Edit(int id)
         {
-            return View();
+            // Especificar la URL del endpoint de la API
+            string baseApiUrl = _configuration.GetSection("WriteMeApi").Value!;
+
+            Post PostInfo = new Post();
+            List<Categoria> categoria = new List<Categoria>();
+
+            using (var client = new HttpClient())
+            {
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage ResPost = await client.GetAsync($"{baseApiUrl}/posts/" + id.ToString());
+                HttpResponseMessage ResCateg = await client.GetAsync($"{baseApiUrl}/categorias/");
+
+                if (ResPost.IsSuccessStatusCode && ResCateg.IsSuccessStatusCode)
+                {
+                    var PostResponse = ResPost.Content.ReadAsStringAsync().Result;
+                    PostInfo = JsonConvert.DeserializeObject<Post>(PostResponse)!;
+
+                    var CategResponse = ResCateg.Content.ReadAsStringAsync().Result;
+                    categoria = JsonConvert.DeserializeObject<List<Categoria>>(CategResponse)!;
+
+                    ViewBag.DropDownData = new SelectList(categoria, "CatId", "CatNombre");
+                }
+
+                return View(PostInfo);
+            }
+
         }
 
         // POST: PostsController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task <ActionResult> Edit(int id, IFormCollection collection)
         {
             try
             {
